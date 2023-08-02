1379d1bc8752e3d93db1bbbca80ce01abf431249
MATHVDT <72647750+MATHVDT@users.noreply.github.com>
2023-08-03 01:12:48 +0200

    lapinHeure.sh correction caractère non ascii

--- clapier_v1/lapinHeure.sh
+++ clapier_v1/lapinHeure.sh
@@ -7,7 +7,7 @@
 # PATH_LAPINE='' # Chemin du script qui permet d'envoyer dans la file le message
 LANG=fr_FR.UTF-8
 
-jour="$(date +"%A %d %b")"
+jour="$(date +"%A %d %b" | iconv -f utf8 -t ascii//TRANSLIT)"
 heure="$(date +%R)"
 
 # Calcule le nombre d'espace nécessaire pour que la ligne fasse 16 caracères
@@ -15,8 +15,8 @@ espace_date=$((16 - ${#heure}))
 espace_date=$((space_needed_date / 2))
 
 # Créée la ligne de 16 caractères
-ligne0="$(printf "%*s%s%*s" "$espace_date" "" "$jour" "$espace_date" "")"
+ligne0="$(printf "%*s%s%*s" "$espace_date" "" "${jour^}" "$espace_date" "")"
 ligne1="$(printf "%*s%s%*s" "5" "" "$heure" "5" "")"
 message_complet="${ligne0};${ligne1}"
 
-${PATH_LAPINE} -m "${message_complet}" -a 5 -x "$(date --date 'now + 1 minutes' +"%Y-%m-%d %H:%M:%S")"
+${PATH_LAPINE} -m "${message_complet}" -a 2 -x "$(date --date 'now + 1 minutes' +"%Y-%m-%d %H:%M:%S")"
